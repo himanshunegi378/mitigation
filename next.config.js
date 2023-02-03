5b39a39accc2c6a48a3409f86301362a6f433f26
Himanshu Negi <himanshunegi378@gmail.com>
2023-02-03 16:57:47 +0530

    basePath /risk_model

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: "/risk_model",
+  basePath: "/risk_model",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
